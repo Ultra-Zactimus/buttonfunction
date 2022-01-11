89417ad66e14224dce0857c6f45213d0b8cb3fc7
Alex Adamovic <alexanderadamovic@gmail.com>
2022-01-11 14:45:05 -0800

    make a website about colors using buttons in javascript
    Co-Authored-By: William Jameson <williamjameson90@gmail.com>
    Co-Authored-By: Zachary Green <ultrazacky@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+
+  $("button#light").click(function() {
+    $("body").removeClass();
+    $("body").addClass("light-background");
+  });
+
+  $("button#dark").click(function() {
+    $("body").removeClass();
+    $("body").addClass("dark-background");
+  });
+
+  $(".highlight").click(function() {
+    $(".highlight").toggleClass("with-highlight");
+    
+  });
+});
